986672b56e10af7647111bf6acd7ebb55018a681
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-04 09:48:06 +0800

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Giovanni Gaffé on 2022/1/4.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
