d69407dcdf847997c03c7451565d57dd56e9b088
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-12 11:13:25 +0800

    set design for card view with rectangle and text with answer

---
+++ Flashzilla/Views/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Giovanni Gaffé on 2022/1/12.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    @State private var isShowingAnswer = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(.white)
+                .shadow(radius: 10)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundColor(.gray)
+                }
+                
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(card: Card.example)
+            .previewInterfaceOrientation(.landscapeRight)
+    }
+}
