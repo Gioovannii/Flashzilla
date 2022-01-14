674a89ba7c869db851a9f55c1ed88dc4234a3c70
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-14 14:44:44 +0800

    add modifier where we can use gesture on cards

--- Flashzilla/Views/ContentView.swift
+++ Flashzilla/Views/ContentView.swift
@@ -47,6 +47,8 @@ struct ContentView: View {
                         }
                         
                         .stacked(at: index, in: cards.count)
+                        .allowsHitTesting((index == cards.count - 1))
+                        .accessibilityHidden(index < cards.count - 1)
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
